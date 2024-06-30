e6124422a4f69982d3616e2cfe76a17f65ecb5f1
Mary-Vivian <muthonimaryvivian>
2024-06-30 19:18:37 +0300

    styled landingpage

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -2,22 +2,25 @@ import './index.css';
 import bag from '../Images/bag.jpg'
 
 
-function Landing(){
-    return(
-        <>   <div className='navbar'>
+function Landing() {
+    return (
+        <>
+            <div className="navbar">
             <button>Search</button>
-       
-                <a>Home</a>
-                <a>Shop</a>
-                <a>Sale</a>
-      
-        </div>
-        <div id='img'>
-        <img  className='landingimg ' src={bag}/>
-        </div>
+            <a>Home</a>
+           <a>Shop</a>
+           <a>Sale</a>  
+            </div>
+            <div className="parent-container"> 
+                <div id="img" className="landingimg">
+                    <img className="landingimg" src={bag} alt="Bag" />
+                </div>
+                <div className="card">
+                    <p className="txt">Free Shopping On order above $300</p>
+                    <button className="shop">Shop Now</button>
+                </div>
+            </div>
         </>
-     
-        
-    )
+    );
 }
 export default Landing;
